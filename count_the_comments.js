8f8fdfa0e1ca6150c4c88e997465ac178d7c764e
Max Tedford <max.tedford@gmail.com>
2015-08-17 17:08:55 -0400

    add count_the_comments solution

---
+++ count_the_comments.js
@@ -0,0 +1,17 @@
+var _ = require("lodash");
+
+var worker = function(collection) {
+  var counted = [];
+  var comments = _.groupBy(collection, 'username');
+  
+  _.forEach(comments, function(comment, name) {
+    counted.push({
+      username: name,
+      comment_count: _.size(comment)
+    })
+  })
+  
+  return counted.reverse();
+};
+
+module.exports = worker;
